afe919d6c9f3943658e99f02a1957e635c4653db
benmarg <benjaminmargolius@gmail.com>
2022-11-22 14:33:48 -0800

    example broken search bar

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -15,7 +15,7 @@ export default function SearchBar(params) {
                     <div className="absolute inset-y-0 left-0 flex items-center">
                         <select id="search-catagory" name="search-catagory" autoComplete="text"
                             value={searchKey}
-                            onChange={(e) => setSearchKey(e.target.value)}
+
                             className="h-full rounded-md border-transparent bg-transparent py-0 pl-3 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                             <option value='first_name' >First Name</option>
                             <option value='last_name' >Last Name</option>
@@ -27,19 +27,19 @@ export default function SearchBar(params) {
                             <option value='linkedin' >LinkedIn</option>
                         </select>
                     </div>
-                    <input type="text" name="search-param" id="search-param" onChange={(e) => setSearchParam(e.target.value)} value={searchParam}
+                    <input type="text" name="search-param" id="search-param"
                         className="block w-[500px] rounded-md border-gray-300 pl-16 focus:border-indigo-500 focus:ring-indigo-500 pl-48 sm:text-sm"
                         placeholder="John Doe" />
                 </div>
                 <button
                     type="button"
                     onClick={() => {
-                        if (searchParam !== lastSearchParam) {
-                            handleSearch();
-                            setLastSearchParam(searchParam);
-                        }
+                        setSearchParam(document.getElementById('search-param').value)
+                        setSearchKey('first_name');
+                        handleSearch()
                     }
-                    } //dont allow the user to search if the search param is the same as the last search param
+                    }
+                    //dont allow the user to search if the search param is the same as the last search param
 
                     className="relative -ml-px inline-flex items-center space-x-2 rounded-md border border-gray-300 bg-gray-50 px-4 ml-6 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
                 >
